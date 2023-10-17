201a7e8617fe2dd5b183ceb15ebb6a70d59b1c9f
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-10-17 15:32:27 -0300

    config docusaurus deploy

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Doc G6',
+  title: 'Chauffeur',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
