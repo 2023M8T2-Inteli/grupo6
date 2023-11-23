2b63d7ea47958361d9c39527063849c425bc12fc
gio-rodrigues0 <giovanna.rodrigues@sou.inteli.edu.br>
2023-11-23 11:54:35 -0300

    creating a node structure for the chat

--- src/workspace/src/bela_tarr/bela_tarr/movement.py
+++ src/workspace/src/bela_tarr/bela_tarr/movement.py
@@ -1,74 +1,53 @@
 #! /usr/bin/env python3 
 import rclpy
+import tf_transformations
+from rclpy.node import Node
 from nav2_simple_commander.robot_navigator import BasicNavigator
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Float32MultiArray
 from tf_transformations import quaternion_from_euler
 from math import pi
 import re
 
-intent_dict = {
-    r"\bponto\s+x\b": "x",
-    r"\bponto\s+y\b": "y",
-    r"\bponto\s+w\b": "w",
-}
+class Navigation(Node):
+    def __init__(self):
+        super().__init__('navigation')
+        self.navigator = BasicNavigator()
+        self.initial_pose = self.create_pose_stamped(self.navigator, 0.0, 0.0, 0.0)
 
-point_dict = {
-    "x": [2.0, 0.0, 0.0],
-    'y': [2.5, 1.0, 0.0],
-    'z': [0.0, 1.0, 0.0],
-}
+        self.nav.waitUntilNav2Active()
+        self.nav.setInitialPose(self.initial_pose)
 
-waypoints = []
+        self.create_subscription(Float32MultiArray, 'waypoints', self.listener_callback, 10)
+        self.subscription
 
-def create_pose_stamped(navigator, pos_x, pos_y, pos_z=0.0):
-    q_x, q_y, q_z, q_w = quaternion_from_euler(0.0, 0.0, pi/4)
-    pose = PoseStamped()
-    pose.header.frame_id = 'map'
-    pose.header.stamp = navigator.get_clock().now().to_msg()
-    pose.pose.position.x = pos_x
-    pose.pose.position.y = pos_y
-    pose.pose.position.z = 0.0
-    pose.pose.orientation.x = q_x
-    pose.pose.orientation.y = q_y
-    pose.pose.orientation.z = q_z
-    pose.pose.orientation.w = q_w
-    return pose
+    def create_pose_stamped(self, navigator, pos_x, pos_y, rot_z):
+        q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, rot_z)
+        self.pose = PoseStamped()
+        self.pose.header.frame_id = 'map'
+        self.pose.header.stamp = navigator.get_clock().now().to_msg()
+        self.pose.pose.position.x = pos_x
+        self.pose.pose.position.y = pos_y
+        self.pose.pose.position.z = pos_x
+        self.pose.pose.orientation.x = q_x
+        self.pose.pose.orientation.y = q_y
+        self.pose.pose.orientation.z = q_z
+        self.pose.pose.orientation.w = q_w
+        return self.pose
+    
+    def listener_callback(self, msg, navigator):
+        command = msg.data
+        x = command[0]
+        y = command[1]
+        self.get_logger().info(f'Going to ({x},{y})')
+        goal_point = self.create_pose_stamped(self.navigator, x, y, 0.0)
+        self.navigator.goToPose(goal_point)
 
-def chatbot(navigator):
-    command = input("Digite o seu comando: ")
-
-    for key, value in intent_dict.items():
-        pattern = re.compile(key)
-        groups = pattern.findall(command)
-        if groups:
-                point = point_dict[value]
-                position_x, position_y, position_z = point
-                print('Coordinate: x={}, y={}, z={}'.format(position_x, position_y, position_z))
-                waypoints.append(create_pose_stamped(navigator, position_x, position_y))
-                break
-
-def main():
-    rclpy.init()
-    nav = BasicNavigator()
-    nav.waitUntilNav2Active()
-
-    initial_pose = create_pose_stamped(nav, 0.0, 0.0)
-
-    nav.setInitialPose(initial_pose)
-
-    question = "y"
-
-    while question.lower() == "y":
-        chatbot(nav)
-        question = input("Mais comandos?(y/n): ")
-
-    nav.followWaypoints(waypoints)
-    while not nav.isTaskComplete():
-        print(nav.getFeedback())
-
-    nav.goToPose(initial_pose)
-
+def main(args=None):
+    rclpy.init(args=args)
+    navigation = Navigation()
+    rclpy.spin(navigation)
+    rclpy.destroy_node()
     rclpy.shutdown()
-
+    
 if __name__ == "__main__":
     main()
