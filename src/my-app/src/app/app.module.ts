3fe6e3445a34c20536b5bc3b571e9e19e629eadd
danielquintaos <99265614+danielquintaos@users.noreply.github.com>
2023-12-05 16:02:56 -0300

    (⌐▀͡ ̯ʖ▀)=/̵͇̿̿/'̿'̿̿̿ ̿ ̿̿

---
+++ src/my-app/src/app/app.module.ts
@@ -0,0 +1,14 @@
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    // ... other declarations
+  ],
+  imports: [
+    // ... other imports
+    FormsModule
+  ],
+  // ... bootstrap and providers
+})
+export class AppModule { }
