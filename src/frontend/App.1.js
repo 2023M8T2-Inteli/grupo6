d2cbd735383260e1434011a1a549dc730517f206
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-12-08 04:37:12 -0300

    nativewind working

--- src/frontend/App.1.js
+++ src/frontend/App.1.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
-      <Text className="text-black text-xs">Tonho is better!</Text>
       <StatusBar style="auto" />
     </View>
   );
