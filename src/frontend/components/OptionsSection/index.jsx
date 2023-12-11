e936fd0a32bd98e0f9fc6d40354f5c305d2c76cd
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-12-11 01:35:44 -0300

    fix options section

--- src/frontend/components/OptionsSection/index.jsx
+++ src/frontend/components/OptionsSection/index.jsx
@@ -3,7 +3,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 function Buttons() {
   return (
-    <View className="shadow-lg bg-white shadow-slate-800 rounded-xl flex flex-row justify-evenly items-center p-1 w-fit">
+    <View className="shadow-lg bg-white shadow-slate-800 rounded-xl flex flex-row justify-evenly items-center p-1 gap-x-4 w-[50vw]">
       <Ionicons name="copy" size={30} color="#E9344E" />
       <AntDesign name="like2" size={30} color="#E9344E" />
       <AntDesign name="dislike2" size={30} color="#E9344E" />
@@ -18,7 +18,7 @@ function Loading() {
 
 export function OptionsSection() {
   return (
-    <View className="w-full flex flex-row items-center justify-start h-min mt-3">
+    <View className="w-full flex flex-row items-center justify-center h-min mt-3">
       <Buttons />
     </View>
   );
