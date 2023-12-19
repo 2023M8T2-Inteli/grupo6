0f0925820053eac66d02ca4abe4285c766793b5c
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-12-19 15:38:25 -0300

    start of context and saprequest

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,13 +2,16 @@ import { Routes } from "./routes";
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
+import { ContextProvider } from "./context/context";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Routes />
-      </GestureHandlerRootView>
+      <ContextProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Routes />
+        </GestureHandlerRootView>
+      </ContextProvider>
     </NavigationContainer>
   );
 }
